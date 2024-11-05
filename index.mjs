f08a3f169239ab540b0d19d69ce5e9e604cfc082
Michael Sprague <michael@sprague.com>
2024-11-05 10:19:10 -0700

    wip

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from "path";
-import Componentry from '@metric-im/componentry'
+import Componentry from '@metric-im/componentry';
+import FireMacro from '../wiki-mixin/components/FireMacro.mjs';
 
 export default class Search extends Componentry.Module {
   constructor(connector,options) {
@@ -20,8 +21,17 @@ export default class Search extends Componentry.Module {
   }
   async register(manifest = []) {
     this.manifest = (Array.isArray(manifest))?manifest:[manifest];
+    await this.load();
   }
+
+  /**
+   * Load the searchable collection according to the fields identified
+   * in the manifest. Listeners will be set on each collection for real
+   * time updates. Invoke load() regularly to ensure searchable data is
+   * current.
+   */
   async load() {
+    //TODO: change to update rather than erase for continuity and efficiency
     await this.collection.remove({});
     let result = [];
     for (let source of this.sources) {
